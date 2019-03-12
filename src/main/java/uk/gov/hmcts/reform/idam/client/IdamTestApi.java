cc97a6fdacd948cec35386cd0d72d238b15f8f36
Tim Jacomb <tim.jacomb@hmcts.net>
2019-03-12 07:51:35 +0000

    Add IdamTestApi
    To make it easier to do functional / integration testing

---
+++ src/main/java/uk/gov/hmcts/reform/idam/client/IdamTestApi.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.idam.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import uk.gov.hmcts.reform.idam.client.models.test.CreateUserRequest;
+
+/**
+ * Client for interacting with idam's user manipulation API
+ * This is only enabled in non production environments
+ */
+@FeignClient(name = "idam-test-api", url = "${idam.api.url}/testing-support", configuration = CoreFeignConfiguration.class)
+public interface IdamTestApi {
+    @PostMapping(value = "/accounts", consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    void createUser(CreateUserRequest createUserRequest);
+
+    @GetMapping("/accounts/pin/{letterHolderId}")
+    String getPinByLetterHolderId(@PathVariable("letterHolderId") String letterHolderId);
+}
