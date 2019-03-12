a8846932705375ef3bc59fbad4ba29faf0c0eca4
Tim Jacomb <tim.jacomb@hmcts.net>
2019-03-12 08:29:53 +0000

    Checkstyle

--- src/main/java/uk/gov/hmcts/reform/idam/client/IdamTestApi.java
+++ src/main/java/uk/gov/hmcts/reform/idam/client/IdamTestApi.java
@@ -11,7 +11,11 @@ import uk.gov.hmcts.reform.idam.client.models.test.CreateUserRequest;
  * Client for interacting with idam's user manipulation API
  * This is only enabled in non production environments
  */
-@FeignClient(name = "idam-test-api", url = "${idam.api.url}/testing-support", configuration = CoreFeignConfiguration.class)
+@FeignClient(
+        name = "idam-test-api",
+        url = "${idam.api.url}/testing-support",
+        configuration = CoreFeignConfiguration.class
+)
 public interface IdamTestApi {
     @PostMapping(value = "/accounts", consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     void createUser(CreateUserRequest createUserRequest);
