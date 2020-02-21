8fdac20294fa5850a41230dd47f32c975ea8d7dd
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-02-21 14:07:34 +0000

    UserInfo deserialization / serialization fix

--- src/main/java/uk/gov/hmcts/reform/idam/client/models/UserInfo.java
+++ src/main/java/uk/gov/hmcts/reform/idam/client/models/UserInfo.java
@@ -3,18 +3,21 @@ package uk.gov.hmcts.reform.idam.client.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class UserInfo {
+public class UserInfo implements Serializable {
     private String sub;
     private String uid;
     private String name;
