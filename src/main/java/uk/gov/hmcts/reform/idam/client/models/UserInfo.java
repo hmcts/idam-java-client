ede98ebf81f1c4894e7af1474260858d99ec066f
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-02-21 14:17:00 +0000

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/idam/client/models/UserInfo.java
+++ src/main/java/uk/gov/hmcts/reform/idam/client/models/UserInfo.java
@@ -3,10 +3,10 @@ package uk.gov.hmcts.reform.idam.client.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
