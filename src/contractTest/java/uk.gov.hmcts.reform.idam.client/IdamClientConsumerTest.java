88aa6ef8c5fcff2a19ac8653b257d2f58cc85305
Dan Hamilton <dan@emagineers.co.uk>
2020-06-16 17:12:14 +0100

    SSCS-7574 update token test

--- src/contractTest/java/uk.gov.hmcts.reform.idam.client/IdamClientConsumerTest.java
+++ src/contractTest/java/uk.gov.hmcts.reform.idam.client/IdamClientConsumerTest.java
@@ -6,6 +6,8 @@ import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.model.RequestResponsePact;
+import com.google.common.collect.Maps;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Map;
@@ -32,41 +36,56 @@ public class IdamClientConsumerTest {
     @Autowired
     private IdamClient idamClient;
 
-    @Pact(state = "provider returns an access token", provider = "Idam_api", consumer = "idamClient")
-    RequestResponsePact getAccessTokenPact(PactDslWithProvider builder) {
-        // @formatter:off
+    private static final String IDAM_OPENID_TOKEN_URL = "/o/token";
+
+    @Pact(provider = "Idam_api", consumer = "Annotation_api")
+    public RequestResponsePact executeGetIdamAccessTokenAndGet200(PactDslWithProvider builder) throws JSONException {
+
+        String[] rolesArray = new String[1];
+        rolesArray[0] = "citizen";
+        Map<String, String> requestheaders = Maps.newHashMap();
+        requestheaders.put("Content-Type", "application/x-www-form-urlencoded");
+
+        Map<String, String> responseheaders = Maps.newHashMap();
+        responseheaders.put("Content-Type", "application/json");
 
         Map<String, Object> params = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-        params.put("username", "emCaseOfficer@email.net");
+        params.put("email", "emCaseOfficer@email.net");
         params.put("password", "Password123");
+        params.put("forename","emCaseOfficer");
+        params.put("surname", "jar123");
+        params.put("roles", rolesArray);
 
         return builder
-                .given("returns a new access token", params)
-                .uponReceiving("a request to POST a citizen")
-                .path("/o/token")
-                .method("POST")
+                .given("a user exists", params)
+                .uponReceiving("Provider takes user/pwd and returns Auth code to Idam Client")
+                .path(IDAM_OPENID_TOKEN_URL)
+                .method(HttpMethod.POST.toString())
+                .body("redirect_uri=https%3A%2F%2Flocalhost%3A5000%2Freceiver&client_id=bsp&grant_type=password&username=emCaseOfficer%40email.net&password=Password123&client_secret=123456&scope=openid profile roles","application/x-www-form-urlencoded")
                 .willRespondWith()
-                .status(201)
-                .matchHeader("Content-Type", "application/json")
-                .body(new PactDslJsonBody()
-                        .stringType("access_token", "some_access_token")
-                        .stringType("expires_in", "1000")
-                        .stringType("id_token", "some_id_token")
-                        .stringType("refresh_token", "some_refresh_token")
-                        .stringType("scope", "openid profile roles")
-                        .stringType("token_type", "bearer")
-                )
+                .status(HttpStatus.OK.value())
+                .headers(responseheaders)
+                .body(createAuthResponse())
                 .toPact();
-        // @formatter:on
     }
 
     @Test
-    @PactTestFor(pactMethod = "getAccessTokenPact")
+    @PactTestFor(pactMethod = "executeGetIdamAccessTokenAndGet200")
     void verifyGetAccessTokenPact() {
 
         String accessToken = idamClient.getAccessToken("emCaseOfficer@email.net", "Password123");
 
-        assertThat(accessToken).isEqualTo("Bearer some_access_token");
+        assertThat(accessToken).isEqualTo("Bearer eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FI");
+    }
 
+    private PactDslJsonBody createAuthResponse() {
+
+        return new PactDslJsonBody()
+                .stringType("access_token", "eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FI")
+                .stringType("refresh_token", "eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FIiwia2lkIjoiYi9PNk92V")
+                .stringType("scope", "openid roles profile")
+                .stringType("id_token", "eyJ0eXAiOiJKV1QiLCJraWQiOiJiL082T3ZWdjEre")
+                .stringType("token_type", "Bearer")
+                .stringType("expires_in","28798");
     }
 }
