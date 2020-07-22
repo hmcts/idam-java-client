d47bfe1158c4899eb6e2ca045b8324444451efaa
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-07-22 09:14:16 +0100

    Added pact test for search api

--- src/contractTest/java/uk.gov.hmcts.reform.idam.client/IdamClientConsumerTest.java
+++ src/contractTest/java/uk.gov.hmcts.reform.idam.client/IdamClientConsumerTest.java
@@ -1,7 +1,10 @@
 package uk.gov.hmcts.reform.idam.client;
 
 import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslJsonRootValue;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
@@ -16,10 +19,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -40,6 +46,11 @@ public class IdamClientConsumerTest {
     private static final String IDAM_OPENID_TOKEN_URL = "/o/token";
 
     private static final String IDAM_DETAILS_URL = "/details";
+
+    private static final String SEARCH_USERS_PATH = "/api/v1/users";
+
+    private static final String ACCESS_TOKEN =
+            "eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FIiwia2lkIjoiRm8rQXAybThDT3ROb290ZjF4TWg0bGc3MFlBPSIsImFsZyI6IlJTMjU2In0.";
 
     @BeforeEach
     public void beforeEach() throws Exception {
@@ -79,6 +90,22 @@ public class IdamClientConsumerTest {
                 .toPact();
     }
 
+    @Pact(provider = "Idam_api", consumer = "idamClient")
+    public RequestResponsePact executeGetSearchUsersAndGet200(PactDslWithProvider builder) throws JSONException {
+
+        return builder
+                .given("Users exist for search")
+                .uponReceiving("Provider receives a GET /api/v1/users with search query")
+                .path(SEARCH_USERS_PATH)
+                .matchQuery("query", ".*", "email:\"bob@hmcts.com\"")
+                .method(HttpMethod.GET.toString())
+                .headers(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
+                .willRespondWith()
+                .status(HttpStatus.OK.value())
+                .body(createSearchUsersResponse())
+                .toPact();
+    }
+
     @Test
     @PactTestFor(pactMethod = "executeGetIdamAccessTokenAndGet200")
     void verifyGetAccessTokenPact() {
@@ -87,6 +114,21 @@ public class IdamClientConsumerTest {
 
         assertThat(returnedAccessToken).isNotNull();
         assertThat(returnedAccessToken).contains("Bearer");
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "executeGetSearchUsersAndGet200")
+    void verifyGetSearchUsers() {
+
+        List<UserDetails> result = idamClient.searchUsers(ACCESS_TOKEN, "email:\"bob@hmcts.com\"");
+
+        assertThat(result).hasSize(1);
+        UserDetails user = result.get(0);
+
+        assertThat(user.getId()).isNotEmpty();
+        assertThat(user.getEmail()).isNotEmpty();
+        assertThat(user.getForename()).isNotEmpty();
+        assertThat(user.getRoles()).isNotEmpty();
     }
 
     private PactDslJsonBody createAuthResponse() {
@@ -101,5 +143,17 @@ public class IdamClientConsumerTest {
                         "eyJ0e.XAiOiJKV1QiLCJra-WQiOiJiL082_T3ZWdjEre")
                 .stringType("token_type", "Bearer")
                 .stringMatcher("expires_in", "[0-9]+", "28798");
+    }
+
+    private DslPart createSearchUsersResponse() {
+
+        return new PactDslJsonArray()
+                .arrayEachLike()
+                .stringMatcher("id", "[a-zA-Z0-9-]+", "a833c2e2-2c73-4900-96ca-74b1efb37928")
+                .stringMatcher("email", "^(.+)@(.+)$", "bob@hmcts.com")
+                .stringType("forename", "Bob")
+                .stringType("surname", "Reform")
+                .minArrayLike("roles", 1, PactDslJsonRootValue.stringType("caseWorker"), 1)
+                .closeObject();
     }
 }
